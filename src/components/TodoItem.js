0ec6b6d1f77fa1568dd114950487007a68da2d64
Admin <hunter17599@gmail.com>
2021-04-06 15:14:53 +0700

    Create by other components

---
+++ src/components/TodoItem.js
@@ -0,0 +1,18 @@
+/* 
+  【TodoItemコンポーネント】
+　・Todoアイテムを表示する
+　・チェックボックスにチェックが入っているか管理する
+　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
+*/
+function TodoItem( {item} ) {
+
+  return (
+    <label className="panel-block">
+        <input type="checkbox" />
+        {item.text}
+
+    </label>
+  );
+}
+
+export default TodoItem;
